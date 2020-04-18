f11c93f7ac630bbe1b826f237ec154cc25da3714
sc5896 <sc5896@gmail.com>
2020-04-18 16:40:59 +0530

    commit-5

--- src/main/java/com/topic/TopicApplication.java
+++ src/main/java/com/topic/TopicApplication.java
@@ -2,12 +2,18 @@ package com.topic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TopicApplication {
+public class TopicApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TopicApplication.class, args);
 	}
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TopicApplication.class);
+    }
 }
